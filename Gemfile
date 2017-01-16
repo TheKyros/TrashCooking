528b6bd0ef59ad13514ac8594b3c6de300c9cfab
Cyril BLAIN <cyrilblain@hotmail.fr>
2017-01-16 16:48:47 +0000

    Recipes CRUD OK with image ! - todo

--- Gemfile
+++ Gemfile
@@ -72,3 +72,6 @@ gem 'chosen-rails'
 gem "select2_rails"
 
 gem 'jquery-datatables-rails', '~> 3.4.0'
+
+## paperclip
+gem "paperclip", "~> 5.0.0"
--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,4 +1,6 @@
 class Recipe < ActiveRecord::Base
    has_and_belongs_to_many :foods
    validates :name, presence: true
+   has_attached_file :photo, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
+   validates_attachment_content_type :photo, content_type: /\Aimage\/.*\z/
 end
