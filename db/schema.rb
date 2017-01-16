b6319166adf821ee81a7a152f3b17748d66708fa
Colin Peremarty <colin.peremarty@yahoo.fr>
2017-01-16 08:12:41 +0000

    Update DB shema

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161213132334) do
+ActiveRecord::Schema.define(version: 20161213102828) do
 
   create_table "categories", force: :cascade do |t|
     t.string   "name"
@@ -37,12 +37,12 @@ ActiveRecord::Schema.define(version: 20161213132334) do
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
     t.string   "last_sign_in_ip"
-    t.datetime "created_at",                          null: false
-    t.datetime "updated_at",                          null: false
     t.string   "confirmation_token"
     t.datetime "confirmed_at"
     t.datetime "confirmation_sent_at"
     t.string   "unconfirmed_email"
+    t.datetime "created_at",                          null: false
+    t.datetime "updated_at",                          null: false
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
