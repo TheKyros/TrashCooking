ecf8688643035623c4b8138e52209f129216576b
Cyril BLAIN <cyrilblain@hotmail.fr>
2017-01-18 09:07:45 +0000

    CleanZoneUser Done

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,4 +3,11 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   before_action :authenticate_user!
+  
+     
+      
+  def authenticate_admin
+    redirect_to root_path, alert: 'Non autorisé.' unless current_user && current_user.isadmin?
+  end
+    
 end
